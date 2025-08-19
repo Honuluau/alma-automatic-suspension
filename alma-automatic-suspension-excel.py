17cdc081dabfa00822d67acc539ddf929c45ffc8
honuluau <honu.dylan@gmail.com>
2025-08-19 10:02:28 -0400

    I did everything at once over a few hours. It helped me focus by not documenting everything every second. I'll work on the workflow my next opportunity.

--- alma-automatic-suspension-excel.py
+++ alma-automatic-suspension-excel.py
@@ -1,5 +1,8 @@
 import pandas as pd
-from datetime import *
+import datetime
+from datetime import date
+import os
+import csv
 from tkinter.filedialog import askopenfilename
 
 current_date = date.today()
@@ -16,7 +19,10 @@ def set_initials():
             initials = new_initials
 
 def get_file_path():
-    return askopenfilename(title="Select Fulfillment - Loans Returns and Overdue Dashboard", filetypes=[("Fulfillment Report", "*.xlsx")])
+    print("Asking for file. Expect pop-up window.")
+    file_path = askopenfilename(title="Select Fulfillment - Loans Returns and Overdue Dashboard", filetypes=[("Fulfillment Report", "*.xlsx")])
+    print(f"Found: {file_path}")
+    return file_path
 
 def get_df_data(file_path):
     df = pd.read_excel(file_path, header=None)
@@ -29,10 +35,15 @@ def iterate_rows_to_form_data(df_data):
     for index, row in df_data.iterrows():
         row_data = row.tolist()
         eagle_id = row_data[0]
-
+        days_overdue = row_data[6]
         if pd.isna(eagle_id):
             # Adding Item
-            print("Adding item.")
+            data[current_id]["items"].append((row_data[8], row_data[7]))
+
+            # Update overdue day
+            if days_overdue > data[current_id]["overdue"]:
+                data[current_id]["overdue"] = days_overdue
+
         else:
             # Creating new data
             current_id = eagle_id
@@ -40,10 +51,66 @@ def iterate_rows_to_form_data(df_data):
             data[eagle_id] = {}
             data[eagle_id]["first_name"] = row_data[1]
             data[eagle_id]["last_name"] = row_data[2]
-            data[eagle_id]["items"] = {}
+            data[eagle_id]["overdue"] = days_overdue
+            data[eagle_id]["items"] = []
 
-            print(f"Adding user data for {eagle_id} ({row_data[1], row_data[2]})")
+            # title, barcode
+            data[eagle_id]["items"].append((row_data[8],row_data[7]))
 
-    input("Test")
+    sorted_data = dict(sorted(data.items(), key=lambda x: x[1]['overdue']))
+    return sorted_data
 
-iterate_rows_to_form_data(get_df_data(get_file_path()))
+# Get Alma-Automatic-Suspension Logs folder
+def get_aasl_folder():
+    output_path = f"{os.path.expanduser("~")}/Documents"
+    folder_name = "Alma-Automatic-Suspension Logs"
+
+    try:
+        os.makedirs(f"{os.path.expanduser("~")}/Documents/{folder_name}")
+        print(f"Created {folder_name} in {output_path}")
+        output_path = f"{output_path}/{folder_name}"
+    except:
+        print(f"Directory found in: {output_path}")
+        return f"{output_path}/{folder_name}"
+
+def format_name(name):
+    name = name.rstrip(" /")
+    return name
+
+def format_suspension_note(items):
+
+    item_string = "[ "
+    for item in items:
+        item_string += f"(\'{format_name(item[0])}\', {item[1]}), "
+    item_string = item_string[:-2] + " ]"
+
+    return f"SUSPENDED / Instance#X / LOST {item_string} -unresolved- {current_date} -{initials}"
+
+def write_csv_log(data, path):
+    rows = [["Eagle Id", "Name", "Longest Overdue (Days)", "Number of Items", "Suspension Note"]]
+    for eagle_id in data:
+        rows.append([
+            eagle_id,
+            f"{data[eagle_id]["last_name"]},"
+            f" {data[eagle_id]["first_name"]}",
+            data[eagle_id]["overdue"],
+            len(data[eagle_id]["items"]),
+            format_suspension_note(data[eagle_id]["items"])
+        ])
+
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    file_path = f"{path}/AAS-{timestamp}.csv"
+    with open(file_path, "w", encoding="utf-8", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(rows)
+
+    try:
+        os.startfile(file_path)
+    except Exception as e:
+        # This has never happened during testing but is a precaution just in case something goes wrong.
+        print("An error occurred while opening the log, you can open it through ~/Documents/Alma-Automatic-Suspension Logs")
+
+path = get_aasl_folder()
+data = iterate_rows_to_form_data(get_df_data(get_file_path()))
+set_initials()
+write_csv_log(data, path)
